72eb104ec1b22915b16211be03074960cf0cd0dc
jaswant7679 <jaswantchaudhary798@gmail.com>
2025-08-12 11:57:47 +0530

    first commit

--- src/pages/Subscription.jsx
+++ src/pages/Subscription.jsx
@@ -5,6 +5,7 @@ import Sectionbuild  from '../components/Subscription/Sectionbuild'
 import  SubscriptionFAQ  from '../components/Subscription/SubscriptionFAQ'
 
 export const Subscription = () => {
+  
   return (
   <>
   <Section/>
